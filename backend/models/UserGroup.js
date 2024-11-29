d8af5180026006cb407dfe52c54beb0d2f297d65
Niles-Rondez <142612332+Niles-Rondez@users.noreply.github.com>
2024-11-29 09:28:10 +0800

    Delete backend directory

--- backend/models/UserGroup.js
+++
@@ -1,7 +0,0 @@
-const { DataTypes } = require("sequelize");
-module.exports = (sequelize) => {
-  return sequelize.define("UserGroup", {
-    userID: { type: DataTypes.INTEGER, allowNull: false },
-    groupID: { type: DataTypes.INTEGER, allowNull: false },
-  });
-};
